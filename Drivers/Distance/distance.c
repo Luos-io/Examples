95259c92942448c6de70b389c2c1afc87b71a899
AntoineVenier <antoine.venier@hotmail.com>
2020-05-13 21:05:00 +0200

    add firmware_revision for distance module

--- Drivers/Distance/distance.c
+++ Drivers/Distance/distance.c
@@ -5,6 +5,12 @@
 
 #define ADDRESS_DEFAULT 0b0101001
 
+#define STRINGIFY(s) STRINGIFY1(s)
+#define STRINGIFY1(s) #s
+
+
+const char FIRM_VERSION[20] = STRINGIFY(VERSION);
+
 VL53L0X_RangingMeasurementData_t ranging_data;
 uint8_t new_data_ready = 0;
 
@@ -52,7 +58,7 @@ void distance_init(void) {
 
     VL53L0X_SetDeviceMode(&dev, VL53L0X_DEVICEMODE_CONTINUOUS_RANGING);
     VL53L0X_StartMeasurement(&dev);
-    luos_module_create(rx_dist_cb, DISTANCE_MOD, "distance_mod");
+    luos_module_create(rx_dist_cb, DISTANCE_MOD, "distance_mod",FIRM_VERSION);
 }
 
 void distance_loop(void) {
