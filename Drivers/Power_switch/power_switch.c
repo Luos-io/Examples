44283c98885873d8ce7bb317d92c36fcf1414ed1
AntoineVenier <antoine.venier@hotmail.com>
2020-05-13 21:27:30 +0200

    add firmware_revision for power_switch module

--- Drivers/Power_switch/power_switch.c
+++ Drivers/Power_switch/power_switch.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include "power_switch.h"
 
+#define STRINGIFY(s) STRINGIFY1(s)
+#define STRINGIFY1(s) #s
+
+
+const char FIRM_VERSION[20] = STRINGIFY(VERSION);
+
 void rx_pow_cb(vm_t *vm, msg_t *msg) {
     if (msg->header.cmd == IO_STATE) {
         HAL_GPIO_WritePin(GPIOA, SWITCH_Pin, msg->data[0]);
@@ -9,7 +15,7 @@ void rx_pow_cb(vm_t *vm, msg_t *msg) {
 }
 
 void power_switch_init(void) {
-    luos_module_enable_rt(luos_module_create(rx_pow_cb, STATE_MOD, "switch_mod"));
+    luos_module_enable_rt(luos_module_create(rx_pow_cb, STATE_MOD, "switch_mod",FIRM_VERSION));
 }
 
 void power_switch_loop(void) {
