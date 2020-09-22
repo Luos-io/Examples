a9b8512aad7e6570b30a511713a675b052aad9a7
Salem-Luos <thomas.salembier@luos.io>
2020-09-22 12:17:43 +0200

    naming and formating driver distance

--- Projects/Distance/src/main.c
+++ Projects/Distance/src/main.c
@@ -99,16 +99,16 @@ int main(void)
   MX_I2C2_Init();
   MX_CRC_Init();
   /* USER CODE BEGIN 2 */
-  luos_init();
-  distance_init();
+  Luos_Init();
+  Distance_Init();
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    luos_loop();
-    distance_loop();
+    Luos_Loop();
+    Distance_Loop();
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
