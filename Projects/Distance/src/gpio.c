80988352c2680d9d433ba416e92941e0002354bc
Salem-Luos <thomas.salembier@luos.io>
2020-09-22 12:17:43 +0200

    [DISTANCE]naming and formating

--- Projects/Distance/src/gpio.c
+++ Projects/Distance/src/gpio.c
@@ -31,6 +31,7 @@
 /* USER CODE END 1 */
 
 /** Configure pins
+     PA2   ------> ADC_IN2
 */
 void MX_GPIO_Init(void)
 {
@@ -58,6 +59,12 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(INT_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PtPin */
+  GPIO_InitStruct.Pin = ROBUS_POWER_SENSOR_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(ROBUS_POWER_SENSOR_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PtPin */
   GPIO_InitStruct.Pin = LED_Pin;
