b8c32813102fb5ed67595a3d233f769ace18403c
Salem-Luos <thomas.salembier@luos.io>
2020-09-22 12:18:35 +0200

    [HANDY]naming and formating

--- Projects/Handy/src/tim.c
+++ Projects/Handy/src/tim.c
@@ -143,11 +143,11 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* timHandle)
   /* USER CODE END TIM2_MspPostInit 0 */
     __HAL_RCC_GPIOA_CLK_ENABLE();
     __HAL_RCC_GPIOB_CLK_ENABLE();
-    /**TIM2 GPIO Configuration    
+    /**TIM2 GPIO Configuration
     PA0     ------> TIM2_CH1
     PB10     ------> TIM2_CH3
     PB11     ------> TIM2_CH4
-    PB3     ------> TIM2_CH2 
+    PB3     ------> TIM2_CH2
     */
     GPIO_InitStruct.Pin = S1_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
@@ -172,10 +172,10 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* timHandle)
   /* USER CODE BEGIN TIM3_MspPostInit 0 */
 
   /* USER CODE END TIM3_MspPostInit 0 */
-  
+
     __HAL_RCC_GPIOB_CLK_ENABLE();
-    /**TIM3 GPIO Configuration    
-    PB5     ------> TIM3_CH2 
+    /**TIM3 GPIO Configuration
+    PB5     ------> TIM3_CH2
     */
     GPIO_InitStruct.Pin = S5_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
@@ -216,7 +216,7 @@ void HAL_TIM_PWM_MspDeInit(TIM_HandleTypeDef* tim_pwmHandle)
 
   /* USER CODE END TIM3_MspDeInit 1 */
   }
-} 
+}
 
 /* USER CODE BEGIN 1 */
 
