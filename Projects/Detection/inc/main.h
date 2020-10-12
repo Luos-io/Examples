acd8a3f56eb22dbbf97ac804f563d0ba71456945
Nicolas Rabault <nicolas.rabault@luos.io>
2020-10-12 16:14:54 +0200

    Clean CubeMX pin names

--- Projects/Detection/inc/main.h
+++ Projects/Detection/inc/main.h
@@ -2,7 +2,6 @@
 #ifndef __MAIN_H
 #define __MAIN_H
 
-
 /*******************************************************************************
  * Includes
  ******************************************************************************/
@@ -16,30 +15,30 @@
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
-#define ROBUS_POWER_SENSOR_Pin GPIO_PIN_2
-#define ROBUS_POWER_SENSOR_GPIO_Port GPIOA
+#define POWER_SENSOR_Pin GPIO_PIN_2
+#define POWER_SENSOR_GPIO_Port GPIOA
 #define LED_Pin GPIO_PIN_3
 #define LED_GPIO_Port GPIOA
-#define RS485_LVL_DOWN_Pin GPIO_PIN_5
-#define RS485_LVL_DOWN_GPIO_Port GPIOA
-#define RS485_LVL_UP_Pin GPIO_PIN_6
-#define RS485_LVL_UP_GPIO_Port GPIOA
+#define COM_LVL_DOWN_Pin GPIO_PIN_5
+#define COM_LVL_DOWN_GPIO_Port GPIOA
+#define COM_LVL_UP_Pin GPIO_PIN_6
+#define COM_LVL_UP_GPIO_Port GPIOA
 #define TX_Pin GPIO_PIN_10
 #define TX_GPIO_Port GPIOB
 #define RX_Pin GPIO_PIN_11
 #define RX_GPIO_Port GPIOB
-#define ROBUS_PTPB_Pin GPIO_PIN_13
-#define ROBUS_PTPB_GPIO_Port GPIOB
-#define ROBUS_RE_Pin GPIO_PIN_14
-#define ROBUS_RE_GPIO_Port GPIOB
-#define ROBUS_DE_Pin GPIO_PIN_15
-#define ROBUS_DE_GPIO_Port GPIOB
-#define ROBUS_PTPA_Pin GPIO_PIN_8
-#define ROBUS_PTPA_GPIO_Port GPIOA
-#define ROBUS_TX_Pin GPIO_PIN_9
-#define ROBUS_TX_GPIO_Port GPIOA
-#define ROBUS_RX_Pin GPIO_PIN_10
-#define ROBUS_RX_GPIO_Port GPIOA
+#define PTPB_Pin GPIO_PIN_13
+#define PTPB_GPIO_Port GPIOB
+#define RX_EN_Pin GPIO_PIN_14
+#define RX_EN_GPIO_Port GPIOB
+#define TX_EN_Pin GPIO_PIN_15
+#define TX_EN_GPIO_Port GPIOB
+#define PTPA_Pin GPIO_PIN_8
+#define PTPA_GPIO_Port GPIOA
+#define COM_TX_Pin GPIO_PIN_9
+#define COM_TX_GPIO_Port GPIOA
+#define COM_RX_Pin GPIO_PIN_10
+#define COM_RX_GPIO_Port GPIOA
 /*******************************************************************************
  * Variables
  ******************************************************************************/
@@ -47,8 +46,6 @@
 /*******************************************************************************
  * Function
  ******************************************************************************/
-void assert_failed(uint8_t* file, uint32_t line);
+void assert_failed(uint8_t *file, uint32_t line);
 
 #endif /* __MAIN_H */
-
-
