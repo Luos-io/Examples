5c1388421df657c1c25d7138191172ebce8f07e0
Nicolas Rabault <nicolas.rabault@luos.io>
2021-02-25 17:02:55 +0100

    [DXL] Use dma for dynamixel communication

--- Projects/l0/Dxl/src/main.c
+++ Projects/l0/Dxl/src/main.c
@@ -20,6 +20,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "dma.h"
 #include "rtc.h"
 #include "usart.h"
 #include "gpio.h"
@@ -90,6 +91,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_USART1_UART_Init();
   MX_USART3_UART_Init();
   MX_RTC_Init();
