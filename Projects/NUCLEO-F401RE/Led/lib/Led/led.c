259486d14020c2759c4aacd3e5c09a7a6fc5a974
Salem_Luos <thomas.salembier@luos.io>
2021-03-09 12:08:35 +0100

    Add example NUCLEO-F401RE

---
+++ Projects/NUCLEO-F401RE/Led/lib/Led/led.c
@@ -0,0 +1,59 @@
+/******************************************************************************
+ * @file Led
+ * @brief driver example a simple Led
+ * @author Luos
+ * @version 0.0.0
+ ******************************************************************************/
+#include "gpio.h"
+#include "led.h"
+
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+uint8_t Led_last_state = 0;
+/*******************************************************************************
+ * Function
+ ******************************************************************************/
+static void Led_MsgHandler(container_t *container, msg_t *msg);
+
+/******************************************************************************
+ * @brief init must be call in project init
+ * @param None
+ * @return None
+ ******************************************************************************/
+void Led_Init(void)
+{
+	revision_t revision = {.unmap = REV};
+    Luos_CreateContainer(Led_MsgHandler, STATE_MOD, "led_mod", revision);
+}
+/******************************************************************************
+ * @brief loop must be call in project loop
+ * @param None
+ * @return None
+ ******************************************************************************/
+void Led_Loop(void)
+{
+}
+/******************************************************************************
+ * @brief Msg manager callback when a msg receive for this container
+ * @param Container destination
+ * @param Msg receive
+ * @return None
+ ******************************************************************************/
+static void Led_MsgHandler(container_t *container, msg_t *msg)
+{
+    if (msg->header.cmd == IO_STATE)
+    {
+        if (msg->data[0] != Led_last_state)
+        {
+            Led_last_state = msg->data[0];
+            HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, Led_last_state);
+        }
+    }
+}
