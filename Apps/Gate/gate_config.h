f0f65d2ac136fb7fe220441b55e7d97ecddd6f5e
Nicolas Rabault <nicolas.rabault@luos.io>
2021-05-24 14:49:05 +0200

    [Gate] Create a gate_config file to simplify app configuration

---
+++ Apps/Gate/gate_config.h
@@ -0,0 +1,11 @@
+#ifndef GATE_CONFIG_H
+#define GATE_CONFIG_H
+
+#ifndef JSON_BUFF_SIZE
+#define JSON_BUFF_SIZE 1024
+#endif
+
+// #define GATE_POLLING
+// #define NODETECTION
+
+#endif /* GATE_CONFIG_H */
