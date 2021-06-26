f01dfc7ec9213500c19311e1ff16b741791e79f6
nicolas cros <nicolas.cros@luos.io>
2021-06-26 15:43:20 +0200

    send message to led profile from button profile

--- Projects/l0/Button/lib/Button/button.c
+++ Projects/l0/Button/lib/Button/button.c
@@ -18,9 +18,12 @@
  * Variables
  ******************************************************************************/
 
-// create a general profile handler
+// create a general core profile handler
 profile_core_t button_profile;
+// create a specific state profile handler
 profile_state_t button;
+
+container_t *app;
 /*******************************************************************************
  * Function
  ******************************************************************************/
@@ -37,7 +40,7 @@ void Button_Init(void)
     // Link state profile to the core profile handler
     Luos_LinkProfile(&button_profile, &button, 0);
     // Container creation following template
-    Luos_LaunchProfile(&button_profile, "button", revision);
+    app = Luos_LaunchProfile(&button_profile, "button", revision);
 }
 
 /******************************************************************************
@@ -48,4 +51,18 @@ void Button_Init(void)
 void Button_Loop(void)
 {
     button.state = (bool)HAL_GPIO_ReadPin(BTN_GPIO_Port, BTN_Pin);
+
+    //Now send a message
+    msg_t led_msg;
+
+    //Get the ID of our LED from the routing table
+    uint8_t id_led = RoutingTB_IDFromAlias("led");
+
+    led_msg.header.target      = id_led;   //We are sending this to the LED
+    led_msg.header.cmd         = IO_STATE; //We are specifying an IO state (on or off)
+    led_msg.header.target_mode = IDACK;    //We are asking for an acknowledgement
+
+    led_msg.header.size = sizeof(char); //Our message only contains one character, the IO state
+    led_msg.data[0]     = button.state; //The I/O state of the LED to be sent (1 or 0, on or off)
+    Luos_SendMsg(app, &led_msg);        //Now that we have the elements, send the message
 }
