9bc519778033315463bc289b3a02dec914dd4c30
nicolas cros <nicolas.cros@luos.io>
2021-06-26 15:07:10 +0200

    add a led service

---
+++ Projects/l0/Button/lib/Led/led.c
@@ -0,0 +1,51 @@
+/******************************************************************************
+ * @file button
+ * @brief driver example a simple button
+ * @author Luos
+ * @version 0.0.0
+ ******************************************************************************/
+#include "led.h"
+#include "gpio.h"
+
+// use profile framework
+#include "profile_state.h"
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+
+// create a general profile handler
+profile_t led_profile;
+profile_state_t led;
+/*******************************************************************************
+ * Function
+ ******************************************************************************/
+
+/******************************************************************************
+ * @brief init must be call in project init
+ * @param None
+ * @return None
+ ******************************************************************************/
+void Led_Init(void)
+{
+    revision_t revision = {.unmap = REV};
+
+    // Link state profile to the general profile handler
+    Luos_LinkProfile(&led_profile, &led, 0);
+    // Container creation following template
+    Luos_LaunchProfile(&led_profile, "led", revision);
+}
+
+/******************************************************************************
+ * @brief loop must be call in project loop
+ * @param None
+ * @return None
+ ******************************************************************************/
+void Led_Loop(void)
+{
+    HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, led.state);
+}
