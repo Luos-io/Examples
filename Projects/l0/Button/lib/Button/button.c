e637ea600a74d196acc764298abf10399e94a3aa
nicolas cros <nicolas.cros@luos.io>
2021-07-28 19:09:21 +0200

    update button with new profile API

--- Projects/l0/Button/lib/Button/button.c
+++ Projects/l0/Button/lib/Button/button.c
@@ -6,7 +6,7 @@
  ******************************************************************************/
 #include "button.h"
 #include "gpio.h"
-#include "template_state.h"
+#include "profile_state.h"
 
 /*******************************************************************************
  * Definitions
@@ -15,8 +15,7 @@
 /*******************************************************************************
  * Variables
  ******************************************************************************/
-template_state_t button_template;
-profile_state_t *button = &button_template.profile;
+profile_state_t button;
 /*******************************************************************************
  * Function
  ******************************************************************************/
@@ -30,9 +29,9 @@ void Button_Init(void)
 {
     revision_t revision = {.major = 1, .minor = 0, .build = 0};
     // Profile configuration
-    button->access = READ_ONLY_ACCESS;
-    // Service creation following template
-    TemplateState_CreateService(0, &button_template, "button", revision);
+    button.access = READ_ONLY_ACCESS;
+    // Service creation following state profile
+    ProfileState_CreateService(&button, 0, "button", revision);
 }
 
 /******************************************************************************
@@ -42,5 +41,5 @@ void Button_Init(void)
  ******************************************************************************/
 void Button_Loop(void)
 {
-    button->state = (bool)HAL_GPIO_ReadPin(BTN_GPIO_Port, BTN_Pin);
+    button.state = (bool)HAL_GPIO_ReadPin(BTN_GPIO_Port, BTN_Pin);
 }
