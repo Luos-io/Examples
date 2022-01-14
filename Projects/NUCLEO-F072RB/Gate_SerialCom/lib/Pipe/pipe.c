e7268554b004a1f38f34269491327c2dd39a67df
Salem_Luos <thomas.salembier@luos.io>
2022-01-14 18:59:06 +0100

    [Pipe] Update for STM32F072

--- Projects/NUCLEO-F072RB/Gate_SerialCom/lib/Pipe/pipe.c
+++ Projects/NUCLEO-F072RB/Gate_SerialCom/lib/Pipe/pipe.c
@@ -51,11 +51,10 @@ void Pipe_Loop(void)
  ******************************************************************************/
 static void Pipe_MsgHandler(service_t *service, msg_t *msg)
 {
-    uint8_t *data = 0;
     uint16_t size = 0;
     if (msg->header.cmd == GET_CMD)
     {
-        if (true == PipeBuffer_GetP2LTask(&data, &size))
+        while (false != PipeBuffer_GetP2LTask(&size))
         {
             // fill the message infos
             msg_t pub_msg;
