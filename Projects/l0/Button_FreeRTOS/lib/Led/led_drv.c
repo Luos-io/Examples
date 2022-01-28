a3f5747c9642e79991fa778a88b3367cc4b8b3e1
Salem_Luos <thomas.salembier@luos.io>
2022-01-28 15:07:37 +0100

    Fix: linting Format ignore on FRTOS

--- Projects/l0/Button_FreeRTOS/lib/Led/led_drv.c
+++ Projects/l0/Button_FreeRTOS/lib/Led/led_drv.c
@@ -31,9 +31,9 @@ void LedDrv_Init(void)
     __HAL_RCC_GPIOA_CLK_ENABLE();
 
     /*Configure GPIO pins : RxEN_Pin */
-    GPIO_InitStruct.Pin = LED_PIN;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Pin   = LED_PIN;
+    GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull  = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     HAL_GPIO_Init(LED_PORT, &GPIO_InitStruct);
 }
