7a1c36e698bc29b817a6789095bf75d72b2ff4b8
biliouriv <biliouriv@gmail.com>
2022-02-02 10:55:04 +0100

    [Inspector] RT filtering

--- Apps/Inspector/pipe_link.c
+++ Apps/Inspector/pipe_link.c
@@ -60,7 +60,24 @@ void PipeLink_Send(service_t *service, void *data, uint32_t size)
  ******************************************************************************/
 uint16_t PipeLink_Find(service_t *service)
 {
-    pipe_id = RoutingTB_IDFromClosestType(service, PIPE_TYPE);
+    search_result_t result;
+    uint8_t localhost = false;
+    // search a pipe type in localhost
+    RTFilter_Node(RTFilter_Type(RTFilter_Reset(&result), PIPE_TYPE), RoutingTB_NodeIDFromID(service->ll_service->id));
+
+    if (result.result_nbr > 0)
+    {
+        // we found a pipe in the same node
+        localhost = true;
+    }
+    else
+    {
+        // pipe is not in the same node
+        RTFilter_Type(RTFilter_Reset(&result), PIPE_TYPE);
+    }
+    // keep pipe_id
+    pipe_id = result.result_table[0]->id;
+
     if (pipe_id > 0)
     {
         // We find one, ask it to auto-update at 1000Hz
@@ -74,7 +91,7 @@ uint16_t PipeLink_Find(service_t *service)
             ;
 
         // Check if pipe is localhost
-        if (RoutingTB_NodeIDFromID(pipe_id) == RoutingTB_NodeIDFromID(service->ll_service->id))
+        if (localhost)
         {
             // This is a localhost pipe
             // Ask for a Streaming channel
