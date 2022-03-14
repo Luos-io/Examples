ddec3f0d854249c6c77072679e09f607581cf03c
nicolas cros <nicolas.cros@luos.io>
2022-03-14 16:45:39 +0100

    use closed loop angle control mode

--- Projects/Arduino/SimpleFOC/lib/motor/motor.cpp
+++ Projects/Arduino/SimpleFOC/lib/motor/motor.cpp
@@ -26,6 +26,7 @@ BLDCDriver3PWM driver = BLDCDriver3PWM(9, 5, 6, 8);
 
 // angular prosition command
 float angle_command = 0.0f;
+float angle_read    = 0.0f;
 /*******************************************************************************
  * Function
  ******************************************************************************/
@@ -42,22 +43,42 @@ void Motor_Init(void)
 
     // initialize driver
     driver.voltage_power_supply = 12;
-    driver.voltage_limit        = 2;
     driver.init();
 
     // initialize motor
     motor.linkSensor(&sensor);
     motor.linkDriver(&driver);
 
-    // currnet = resistance*voltage, so try to be well under 1Amp
-    motor.voltage_limit  = 0.8;  // [V]
-    motor.velocity_limit = 12.0; // [rad/s] cca 50rpm
+    // choose FOC modulation (optional)
+    motor.foc_modulation = FOCModulationType::SpaceVectorPWM;
 
-    // open loop control config
-    motor.controller = MotionControlType::angle_openloop;
+    // set motion control loop to be used
+    motor.controller = MotionControlType::angle;
+    // motor.controller = MotionControlType::velocity;
+
+    // contoller configuration
+    // default parameters in defaults.h
+
+    // velocity PI controller parameters
+    motor.PID_velocity.P = 0.2f;
+    motor.PID_velocity.I = 20;
+    motor.PID_velocity.D = 0;
+    // maximal voltage to be set to the motor
+    motor.voltage_limit = 2.0;
+
+    // velocity low pass filtering time constant
+    // the lower the less filtered
+    motor.LPF_velocity.Tf = 0.01f;
+
+    // angle P controller
+    motor.P_angle.P = 20;
+    // maximal velocity of the position control
+    motor.velocity_limit = 20.0;
 
     // init motor hardware
     motor.init();
+    // align sensor and start FOC
+    motor.initFOC();
 
     // initialize service
     revision_t revision = {1, 0, 0};
@@ -70,11 +91,21 @@ void Motor_Init(void)
  ******************************************************************************/
 void Motor_Loop(void)
 {
+    // main FOC algorithm function
+    // the faster you run this function the better
+    // Arduino UNO loop  ~1kHz
+    // Bluepill loop ~10kHz
+    motor.loopFOC();
+
     // Motion control function
     // velocity, position or voltage (defined in motor.controller)
     // this function can be run at much lower frequency than loopFOC() function
     // You can also use motor.move() and set the motor.target in the code
     motor.move(AngularOD_PositionTo_rad(angle_command) * GEAR_RATE);
+
+    // update sensor position
+    // don't know why there is a *5.7 coef to get the right value
+    angle_read = sensor.getAngle() * 5.724417123;
 }
 /******************************************************************************
  * @brief Msg Handler call back when a msg receive for this service
@@ -87,5 +118,16 @@ static void Motor_MsgHandler(service_t *service, msg_t *msg)
     if ((msg->header.cmd == ANGULAR_POSITION))
     {
         AngularOD_PositionFromMsg((angular_position_t *)&angle_command, msg);
+    }
+
+    if ((msg->header.cmd == GET_CMD))
+    {
+        // Report management
+        msg_t pub_msg;
+        pub_msg.header.target_mode = msg->header.target_mode;
+        pub_msg.header.target      = msg->header.source;
+
+        AngularOD_PositionToMsg((angular_position_t *)&angle_read, &pub_msg);
+        Luos_SendMsg(service, &pub_msg);
     }
 }
